51af25981d9d61222774429245aee22a628e96b5
Alexandre Bulté <alexandre@bulte.net>
2024-10-16 16:35:38 +0200

    test query generation

--- metrics.py
+++ metrics.py
@@ -1,11 +1,16 @@
 from db import query
 
 
-def compute_quality_score(organization: str | None = None) -> float | None:
+def quality_score_query(organization: str | None = None) -> tuple[str, dict]:
     kwargs = {}
     q = "SELECT AVG((quality->>'score')::numeric) AS mean_score FROM catalog"
     if organization:
         q = f"{q} WHERE organization = :org"
         kwargs["org"] = organization
+    return q, kwargs
+
+
+def compute_quality_score(organization: str | None = None) -> float | None:
+    q, kwargs = quality_score_query(organization)
     avg_quality__score = next(query(q, **kwargs))
     return avg_quality__score["mean_score"] if avg_quality__score else None
--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,6 +1,6 @@
 from unittest.mock import patch
 
-from metrics import compute_quality_score
+from metrics import compute_quality_score, quality_score_query
 
 
 def test_compute_quality_score():
@@ -10,3 +10,14 @@ def test_compute_quality_score():
 
     with patch("db.get", MockDb):
         assert compute_quality_score() == 0.5
+
+
+def test_quality_score_query():
+    q, kwargs = quality_score_query()
+    assert "mean_score" in q
+    assert kwargs == {}
+
+    o_q, o_kwargs = quality_score_query(organization="org")
+    assert q in o_q
+    assert "organization =" in o_q
+    assert o_kwargs == {"org": "org"}
