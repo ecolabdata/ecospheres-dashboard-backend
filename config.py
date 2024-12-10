3b0885269941d54ed24ae677e53f27b74e3d1e7a
Alexandre Bulté <alexandre@bulte.net>
2024-12-10 15:26:27 +0100

    switch org_api to main

--- config.py
+++ config.py
@@ -22,8 +22,7 @@ ENVS_CONF: dict[Literal["prod", "demo"], ConfigDict] = {
         "stats_url": "https://stats.data.gouv.fr/index.php",
         "stats_site_id": "299",
         "stats_token": os.getenv("STATS_TOKEN", ""),
-        # FIXME: move to main branch when available
-        "org_api": "https://raw.githubusercontent.com/ecolabdata/ecospheres-universe/refs/heads/feat/org-api/dist/organizations-prod.json",
+        "org_api": "https://raw.githubusercontent.com/ecolabdata/ecospheres-universe/refs/heads/main/dist/organizations-prod.json",
     },
     "demo": {
         "universe_name": "ecospheres",
@@ -33,8 +32,7 @@ ENVS_CONF: dict[Literal["prod", "demo"], ConfigDict] = {
         "stats_url": None,
         "stats_site_id": None,
         "stats_token": None,
-        # FIXME: move to main branch when available
-        "org_api": "https://raw.githubusercontent.com/ecolabdata/ecospheres-universe/refs/heads/feat/org-api/dist/organizations-demo.json",
+        "org_api": "https://raw.githubusercontent.com/ecolabdata/ecospheres-universe/refs/heads/main/dist/organizations-demo.json",
     },
 }
 
