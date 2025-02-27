f6a34ad1719ac067927405a28417d8de5b05a62e
streino <streino@disroot.org>
2025-02-27 01:07:01 +0100

    Recreate migration from up-to-date repo

--- alembic/versions/84c4d5a939e5_contact_points_as_list.py
+++ alembic/versions/84c4d5a939e5_contact_points_as_list.py
@@ -1,8 +1,8 @@
 """contact_points as list
 
-Revision ID: 53a7784891b6
-Revises: af780a5cffbe
-Create Date: 2025-02-21 12:55:31.873698
+Revision ID: 84c4d5a939e5
+Revises: 4eb628a9e208
+Create Date: 2025-02-27 01:02:56.425913
 
 Changes:
 - Migrate dataset.contact_point to dataset.contact_points[]
@@ -16,8 +16,8 @@ from sqlalchemy.dialects.postgresql import JSONB
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = "53a7784891b6"
-down_revision: Union[str, None] = "af780a5cffbe"
+revision: str = "84c4d5a939e5"
+down_revision: Union[str, None] = "4eb628a9e208"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
