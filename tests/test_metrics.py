503be5b4f870c10bc1595719383ecada75c75b3e
streino <streino@disroot.org>
2025-07-01 14:31:54 +0200

    Fix broken test

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,5 +1,4 @@
 from datetime import date
-from unittest.mock import patch
 
 import pytest
 import requests_mock
@@ -36,15 +35,16 @@ def test_quality_score_query():
     assert o_kwargs == {"org": "org"}
 
 
-@patch("datetime.date")
-def test_get_datagouvfr_metrics(mock_date, mock_requests):
+def test_get_datagouvfr_metrics(mock_requests):
     url = "https://example.com/api"
+    now = date.today()
+    metrics_month = f"{now.year}-{str(now.month - 1).zfill(2)}"
     mock_response = {
         "data": [
             {
                 "__id": 50271644,
                 "dataset_id": "5c4ae55a634f4117716d5656",
-                "metric_month": "2025-05",
+                "metric_month": metrics_month,
                 "monthly_visit": 59994,
                 "monthly_download_resource": 9342,
             }
@@ -53,18 +53,9 @@ def test_get_datagouvfr_metrics(mock_date, mock_requests):
         "meta": {"page": 1, "page_size": 20, "total": 1},
     }
     mock_requests.get(url, json=mock_response)
-    mock_date.today.return_value = date(2025, 6, 1)
     result = get_datagouvfr_metrics(url, {})
-    assert result == [
-        {
-            "__id": 50271644,
-            "dataset_id": "5c4ae55a634f4117716d5656",
-            "metric_month": "2025-05",
-            "monthly_visit": 59994,
-            "monthly_download_resource": 9342,
-        }
-    ]
-    assert mock_requests.request_history[0].qs == {"metric_month__exact": ["2025-05"]}
+    assert result == mock_response["data"]
+    assert mock_requests.request_history[0].qs == {"metric_month__exact": [metrics_month]}
 
 
 def test_get_datagouvfr_metrics_not_found(mock_requests):
