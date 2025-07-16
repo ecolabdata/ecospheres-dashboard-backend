ad242797a94b3051a58045d1f00885b995059dca
streino <30210525+streino@users.noreply.github.com>
2025-07-16 17:15:01 +0200

    fix: Exclude dicts with only null values as part of DEFAULT_JSON_EXCLUDE (#60)
    This means {"foo": None, "bar": None} is considered as an empty dict and is excluded by default when checking whether a field "exists".

--- utils.py
+++ utils.py
@@ -1,18 +1,46 @@
 import math
 import re
 import time
-from typing import TypeAlias, TypedDict, TypeVar
+from collections.abc import Sequence
+from typing import Any, Protocol, TypedDict
 
 import requests
 from sqlalchemy.orm import scoped_session
 
-from models import Bouquet, Dataset, DatasetMetric, Metric, Organization, Resource, Stats
 
-Model: TypeAlias = Bouquet | Dataset | DatasetMetric | Metric | Organization | Resource | Stats
-T = TypeVar("T", bound=Model)
+def no_value_dict(obj: Any) -> bool:
+    return isinstance(obj, dict) and all(v is None for v in obj.values())
 
 
-def upsert(session: scoped_session, new: T, existing: T | None, auto_commit: bool = True) -> T:
+DEFAULT_EXCLUDE = (None,)
+DEFAULT_JSON_EXCLUDE = (None, {}, no_value_dict)
+DEFAULT_LIST_EXCLUDE = (None, [])
+DEFAULT_STRING_EXCLUDE = (None, "")
+
+
+def accept(element: Any, exclude: Sequence[Any] = DEFAULT_EXCLUDE) -> bool:
+    """
+    Return True if `element` is not in the `exclude` sequence, False otherwise.
+
+    The `exclude` sequence can contain:
+    - A value which should be excluded.
+    - A callable taking `element` as a single parameter and returning True iff `element` should be excluded.
+    """
+    for item in exclude:
+        if callable(item) and item(element):
+            return False
+        elif element == item:
+            return False
+    return True
+
+
+class HasId(Protocol):
+    id: Any
+
+
+def upsert[T: HasId](
+    session: scoped_session, new: T, existing: T | None, auto_commit: bool = True
+) -> T:
     if existing:
         new.id = existing.id
         session.merge(new)
